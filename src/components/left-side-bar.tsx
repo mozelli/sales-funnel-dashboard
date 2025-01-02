1f76e2495473ecfa7a66ceeddbc7445b237df1ce
João Mozelli Neto <joaomozelli@gmail.com>
2025-01-02 14:48:43 -0300

    implements left side bar

--- src/components/left-side-bar.tsx
+++ src/components/left-side-bar.tsx
@@ -4,42 +4,56 @@ import { Plus, StickyNote } from "lucide-react";
 import { Button } from "./ui/button";
 
 const LeftSideBar = () => {
-  const { sections, setSection } = useProject();
-  const [sectionName, setSectionName] = useState("");
+  const { id, steps, setStep } = useProject();
+  const [stepName, setStepName] = useState("");
 
-  const addNewPage = () => {
-    setSection(sectionName);
-    setSectionName("");
+  const addNewStep = () => {
+    setStep({ id: "", project_id: id, name: stepName, nodes: [] });
+    setStepName("");
   };
+
   return (
-    <div className="w-[400px] p-1">
-      <div className="flex gap-1">
-        <input
-          placeholder="Criar nova etapa"
-          type="text"
-          value={sectionName}
-          onChange={(e) => setSectionName(e.target.value)}
-          className="border-2 rounded-sm p-1 outline-none"
-        />
-        <Button
-          variant={"outline"}
-          className="border-indigo-200 border-2 rounded-sm"
-          onClick={() => addNewPage()}
-        >
-          <Plus />
-        </Button>
+    <div className="flex w-[400px] p-1">
+      <div className="flex flex-col w-[300px] border-r">
+        <div className="flex gap-1">
+          <input
+            placeholder="Criar nova etapa"
+            type="text"
+            value={stepName}
+            onChange={(e) => setStepName(e.target.value)}
+            className="border-2 rounded-sm p-1 outline-none"
+          />
+          {
+            <Button
+              variant={"outline"}
+              className="border-indigo-200 border-2 rounded-sm"
+              onClick={() => addNewStep()}
+            >
+              <Plus />
+            </Button>
+          }
+        </div>
+        <div className="">
+          <ul>
+            {steps.map((step, index) => {
+              return (
+                <li key={index} className="p-2 flex items-center gap-1">
+                  <StickyNote />
+                  <span>{step.name}</span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
-      <div className="">
-        <ul>
-          {sections.map((section, index) => {
-            return (
-              <li key={index} className="p-2 flex items-center gap-1">
-                <StickyNote />
-                <span>{section.name}</span>
-              </li>
-            );
-          })}
-        </ul>
+      <div
+        className={`flex flex-col items-center w-[100px] pl-1 ${
+          steps.length === 0 ? "hidden" : "block"
+        }`}
+      >
+        <Button className="w-full" size={"sm"}>
+          Container
+        </Button>
       </div>
     </div>
   );
