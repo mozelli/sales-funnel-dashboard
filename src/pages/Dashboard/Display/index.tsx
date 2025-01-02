8a4214f2a59f2cabadb34c66e6206df6bc60f82c
João Mozelli Neto <joaomozelli@gmail.com>
2025-01-02 19:52:19 -0300

    implements component constructor

--- src/pages/Dashboard/Display/index.tsx
+++ src/pages/Dashboard/Display/index.tsx
@@ -1,5 +1,13 @@
+import { useEffect, useRef, useState } from "react";
+
 import { userFunnelStore } from "../../../store/project";
 import { Button } from "../../../components/ui/button";
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "../../../components/ui/alert";
+import { Terminal } from "lucide-react";
 
 type Attributes = {
   key: string;
@@ -12,11 +20,55 @@ type Attributes = {
 
 const Display = () => {
   const { funnel, currentStage } = userFunnelStore();
+  const [activeDiv, setActiveDiv] = useState<string | null>(null);
+  const divRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        divRefs.current.every(
+          (ref) => ref && !ref.contains(event.target as Node)
+        )
+      ) {
+        setActiveDiv(null);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const addRef = (ref: HTMLDivElement | null) => {
+    if (ref && !divRefs.current.includes(ref)) {
+      divRefs.current.push(ref);
+    }
+  };
 
   const componentConstructor = (type: string, attributes: Attributes) => {
     switch (type) {
       case "button":
         return <Button key={attributes.key}>Botão</Button>;
+      case "alert":
+        return (
+          <div
+            ref={addRef}
+            onClick={() => setActiveDiv(attributes.key)}
+            className={
+              activeDiv === attributes.key
+                ? "border-2 border-cyan-500 border-dashed"
+                : ""
+            }
+            key={attributes.key}
+          >
+            <Alert>
+              <Terminal className="h-4 w-4" />
+              <AlertTitle>Título</AlertTitle>
+              <AlertDescription>Descrição</AlertDescription>
+            </Alert>
+          </div>
+        );
       default:
         return "";
     }
@@ -24,7 +76,7 @@ const Display = () => {
 
   return (
     <div className="flex-1 h-full bg-slate-100 pt-2 pb-2">
-      <div className={`bg-white shadow h-full w-[430px] m-auto`}>
+      <div className={`bg-white shadow h-full w-[430px] m-auto p-1`}>
         {currentStage !== ""
           ? funnel.stages.map((stage) =>
               stage.id === currentStage
