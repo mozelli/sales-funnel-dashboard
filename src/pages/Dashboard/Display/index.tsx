09b63fc924502cd1eaa711de45bfacda7d53aeaf
João Mozelli Neto <joaomozelli@gmail.com>
2025-01-14 11:35:00 -0300

    refactoring Display

--- src/pages/Dashboard/Display/index.tsx
+++ src/pages/Dashboard/Display/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 
-import { useFunnelStore } from "../../../store/project";
 import { Button } from "../../../components/ui/button";
 import {
   Alert,
@@ -9,17 +8,14 @@ import {
 } from "../../../components/ui/alert";
 import { Terminal } from "lucide-react";
 
-type Attributes = {
-  key: string;
-  src?: string;
-  alt?: string;
-  href?: string;
-  class?: string;
-  textContent?: string;
-};
+import { useFunnelStore } from "../../../store/funnelState";
+import { useDashboardStore } from "../../../store/dashboardState";
+import { Node } from "../../../types/Node";
 
 const Display = () => {
   const { funnel, currentStage } = useFunnelStore();
+  const { setMenu } = useDashboardStore();
+
   const [activeDiv, setActiveDiv] = useState<string | null>(null);
   const divRefs = useRef<(HTMLDivElement | null)[]>([]);
 
@@ -31,6 +27,7 @@ const Display = () => {
         )
       ) {
         setActiveDiv(null);
+        //setMenu("componentsMenu");
       }
     };
 
@@ -46,21 +43,55 @@ const Display = () => {
     }
   };
 
-  const componentConstructor = (type: string, attributes: Attributes) => {
-    switch (type) {
+  const stringfyAttributesClass = (node: Node) => {
+    let result = "";
+    //if (node.attributes.class !== undefined) {
+    if (Array.isArray(node.attributes.class)) {
+      const classArray = node.attributes.class;
+      result = classArray.join(" ");
+    }
+    return result;
+  };
+
+  const componentConstructor = (node: Node) => {
+    switch (node.type) {
+      case "title":
+        return (
+          <div
+            ref={addRef}
+            onClick={() => {
+              setActiveDiv(node.attributes.key);
+              setMenu("editTitle");
+            }}
+            className={`text-2xl font-bold ${
+              activeDiv === node.attributes.key
+                ? "outline-dashed outline-cyan-300"
+                : ""
+            }`}
+            key={node.attributes.key}
+          >
+            <h1
+              className={`text-2xl font-bold ${stringfyAttributesClass(node)}`}
+            >
+              Título
+            </h1>
+          </div>
+        );
+        break;
       case "button":
-        return <Button key={attributes.key}>Botão</Button>;
+        return <Button key={node.attributes.key}>Botão</Button>;
+        break;
       case "alert":
         return (
           <div
             ref={addRef}
-            onClick={() => setActiveDiv(attributes.key)}
+            onClick={() => setActiveDiv(node.attributes.key)}
             className={
-              activeDiv === attributes.key
+              activeDiv === node.attributes.key
                 ? "border-2 border-cyan-500 border-dashed"
                 : ""
             }
-            key={attributes.key}
+            key={node.attributes.key}
           >
             <Alert>
               <Terminal className="h-4 w-4" />
@@ -69,6 +100,7 @@ const Display = () => {
             </Alert>
           </div>
         );
+        break;
       default:
         return "";
     }
@@ -76,12 +108,12 @@ const Display = () => {
 
   return (
     <div className="flex-1 h-full bg-slate-100 pt-2 pb-2">
-      <div className={`bg-white shadow h-full w-[430px] m-auto p-1`}>
+      <div className={`bg-white shadow h-full w-[900px] m-auto p-1`}>
         {currentStage !== ""
           ? funnel.stages.map((stage) =>
               stage.id === currentStage
                 ? stage.nodes.map((node) => {
-                    return componentConstructor(node.type, node.attributes);
+                    return componentConstructor(node);
                   })
                 : ""
             )
