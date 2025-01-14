ffe805124f64f5049649d90de0912cf3dd8b3889
João Mozelli Neto <joaomozelli@gmail.com>
2025-01-14 11:36:08 -0300

    modify attributes class type

--- src/types/Node.ts
+++ src/types/Node.ts
@@ -1,13 +1,19 @@
 export type Node = {
   id: string;
   type: string;
-  attributes: {
-    key: string;
-    src?: string;
-    alt?: string;
-    href?: string;
-    class?: string;
-    textContent?: string;
-  };
+  attributes: Attributes;
   children: Node[];
 };
+
+export type Attributes = {
+  key: string;
+  src?: string;
+  alt?: string;
+  href?: string;
+  class?: string[];
+  textContent?: string;
+};
+
+/*export type AttributeClass = {
+  width?: string;
+};*/
