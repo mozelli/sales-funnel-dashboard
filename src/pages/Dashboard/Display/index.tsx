43e2cca9c19ddfb1108b653608ddd67716c5df02
João Mozelli Neto <joaomozelli@gmail.com>
2025-01-15 09:26:37 -0300

    change width size layout

--- src/pages/Dashboard/Display/index.tsx
+++ src/pages/Dashboard/Display/index.tsx
@@ -81,7 +81,7 @@ const Display = () => {
 
   return (
     <div className="flex-1 h-full bg-slate-100 pt-2 pb-2">
-      <div className={`bg-white shadow h-full w-[900px] m-auto p-1`}>
+      <div className={`bg-white shadow h-full w-[360px] m-auto p-1`}>
         {currentStage !== ""
           ? funnel.stages.map((stage) =>
               stage.id === currentStage
