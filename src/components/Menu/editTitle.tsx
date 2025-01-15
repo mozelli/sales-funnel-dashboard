8f0e61c178dd9cc20304dc879aa4ca5df83ef2c0
João Mozelli Neto <joaomozelli@gmail.com>
2025-01-15 15:25:32 -0300

    implements new funtionalities for editTitle

--- src/components/Menu/editTitle.tsx
+++ src/components/Menu/editTitle.tsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from "react";
 
 import { Separator } from "../ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
 import { AlignLeft, AlignCenter, AlignRight } from "lucide-react";
-//import clsx from "clsx";
-//import { cn } from "../../lib/utils";
 
 import { useFunnelStore } from "../../store/funnelState";
 import { TitleClass } from "../../types/Node";
 import { useDashboardStore } from "../../store/dashboardState";
-//import { TitleClass } from "../../types/Node";
-//import { Attributes } from "../../types/Node";
 
 const EditTitle = () => {
-  const { funnel, currentNode, currentStage, updateNodeAttributeClass } =
-    useFunnelStore();
+  const {
+    funnel,
+    currentNode,
+    currentStage,
+    updateNodeAttributeClass,
+    updateNodeAttributeTextContent,
+  } = useFunnelStore();
 
   const { colors } = useDashboardStore();
 
@@ -23,12 +25,21 @@ const EditTitle = () => {
     letterSpacing: "",
     textAlign: "",
     color: "",
+    paddingTop: "",
+    paddingRight: "",
+    paddingBottom: "",
+    paddingLeft: "",
   });
+  const [textContent, setTextContent] = useState<string | undefined>("");
   const [size, setSize] = useState("");
   const [weight, setWeight] = useState("");
   const [letterSpacing, setLetterSpacing] = useState("");
   const [textAlign, setTextAlign] = useState("");
   const [color, setColor] = useState("");
+  const [paddingTop, setPaddingTop] = useState("");
+  const [paddingRight, setPaddingRight] = useState("");
+  const [paddingBottom, setPaddingBottom] = useState("");
+  const [paddingLeft, setPaddingLeft] = useState("");
 
   useEffect(() => {
     const getClassAttribute = () => {
@@ -39,12 +50,17 @@ const EditTitle = () => {
               node.id === currentNode &&
               node.attributes.class !== undefined
             ) {
+              setTextContent(node.attributes.textContent);
               setNodeClass(node.attributes.class);
               setSize(node.attributes.class.size);
               setWeight(node.attributes.class.weight);
               setLetterSpacing(node.attributes.class.letterSpacing);
               setTextAlign(node.attributes.class.textAlign);
               setColor(node.attributes.class.color);
+              setPaddingTop(node.attributes.class.paddingTop);
+              setPaddingRight(node.attributes.class.paddingRight);
+              setPaddingBottom(node.attributes.class.paddingBottom);
+              setPaddingLeft(node.attributes.class.paddingLeft);
             }
           });
         }
@@ -52,6 +68,11 @@ const EditTitle = () => {
     };
     getClassAttribute();
   }, [currentNode, currentStage, funnel]);
+
+  const handleTextContent = (textContent: string) => {
+    setTextContent(textContent);
+    updateNodeAttributeTextContent(currentStage, currentNode, textContent);
+  };
 
   const handleSize = (size: string) => {
     setSize(size);
@@ -62,6 +83,7 @@ const EditTitle = () => {
       letterSpacing: nodeClass.letterSpacing,
       textAlign: nodeClass.textAlign,
       color: nodeClass.color,
+      paddingTop: nodeClass.paddingTop,
     });
   };
 
@@ -74,6 +96,7 @@ const EditTitle = () => {
       letterSpacing: nodeClass.letterSpacing,
       textAlign: nodeClass.textAlign,
       color: nodeClass.color,
+      paddingTop: nodeClass.paddingTop,
     });
   };
 
@@ -86,6 +109,7 @@ const EditTitle = () => {
       letterSpacing,
       textAlign: nodeClass.textAlign,
       color: nodeClass.color,
+      paddingTop: nodeClass.paddingTop,
     });
   };
 
@@ -98,6 +122,7 @@ const EditTitle = () => {
       letterSpacing: nodeClass.letterSpacing,
       textAlign,
       color: nodeClass.color,
+      paddingTop: nodeClass.paddingTop,
     });
   };
 
@@ -110,17 +135,93 @@ const EditTitle = () => {
       letterSpacing: nodeClass.letterSpacing,
       textAlign: nodeClass.textAlign,
       color: `text-${color}`,
+      paddingTop: nodeClass.paddingTop,
+    });
+  };
+
+  const handlePaddingTop = (paddingTop: string) => {
+    setPaddingTop(paddingTop);
+    updateNodeAttributeClass(currentStage, currentNode, {
+      ...nodeClass,
+      size: nodeClass.size,
+      weight: nodeClass.weight,
+      letterSpacing: nodeClass.letterSpacing,
+      textAlign: nodeClass.textAlign,
+      color: nodeClass.color,
+      paddingTop,
+    });
+  };
+
+  const handlePaddingRight = (paddingRight: string) => {
+    setPaddingRight(paddingRight);
+    updateNodeAttributeClass(currentStage, currentNode, {
+      ...nodeClass,
+      size: nodeClass.size,
+      weight: nodeClass.weight,
+      letterSpacing: nodeClass.letterSpacing,
+      textAlign: nodeClass.textAlign,
+      color: nodeClass.color,
+      paddingTop: nodeClass.paddingTop,
+      paddingRight,
+    });
+  };
+
+  const handlePaddingBottom = (paddingBottom: string) => {
+    setPaddingBottom(paddingBottom);
+    updateNodeAttributeClass(currentStage, currentNode, {
+      ...nodeClass,
+      size: nodeClass.size,
+      weight: nodeClass.weight,
+      letterSpacing: nodeClass.letterSpacing,
+      textAlign: nodeClass.textAlign,
+      color: nodeClass.color,
+      paddingTop: nodeClass.paddingTop,
+      paddingRight: nodeClass.paddingRight,
+      paddingBottom,
+    });
+  };
+
+  const handlePaddingLeft = (paddingLeft: string) => {
+    setPaddingLeft(paddingLeft);
+    updateNodeAttributeClass(currentStage, currentNode, {
+      ...nodeClass,
+      size: nodeClass.size,
+      weight: nodeClass.weight,
+      letterSpacing: nodeClass.letterSpacing,
+      textAlign: nodeClass.textAlign,
+      color: nodeClass.color,
+      paddingTop: nodeClass.paddingTop,
+      paddingRight: nodeClass.paddingRight,
+      paddingBottom: nodeClass.paddingBottom,
+      paddingLeft,
     });
   };
 
   return (
-    <div className="">
+    <ScrollArea className="w-full h-[500px] pl-1 pr-2">
       <div className="text-slate-600 text-xs font-bold m-1 text-center">
         Editar título
       </div>
       <Separator className="my-2" />
+      <div className="flex flex-col items-center justify-between m-1">
+        <label htmlFor="textContent" className="text-sm font-medium">
+          Texto
+        </label>
+        <textarea
+          className="border rounded-sm text-sm w-full p-1"
+          id="textContent"
+          value={textContent}
+          onChange={(e) => {
+            setTextContent(e.target.value);
+            handleTextContent(e.target.value);
+          }}
+        >
+          {textContent}
+        </textarea>
+      </div>
+      <Separator className="my-2" />
       <div className="flex items-center justify-between m-1">
-        <label htmlFor="fontSize" className="text-sm">
+        <label htmlFor="fontSize" className="text-sm font-medium">
           Tamanho
         </label>
         <select
@@ -148,7 +249,7 @@ const EditTitle = () => {
       </div>
       <Separator className="my-2" />
       <div className="flex items-center justify-between m-1">
-        <label htmlFor="fontWeight" className="text-sm">
+        <label htmlFor="fontWeight" className="text-sm font-medium">
           Espessura
         </label>
         <select
@@ -172,7 +273,7 @@ const EditTitle = () => {
       </div>
       <Separator className="my-2" />
       <div className="flex items-center justify-between m-1">
-        <label htmlFor="letterSpacing" className="text-sm">
+        <label htmlFor="letterSpacing" className="text-sm font-medium">
           Letter spacing
         </label>
         <select
@@ -193,7 +294,7 @@ const EditTitle = () => {
       </div>
       <Separator className="my-2" />
       <div className="flex items-center justify-between m-1">
-        <label htmlFor="textAlign" className="text-sm">
+        <label htmlFor="textAlign" className="text-sm font-medium">
           Alinhamento
         </label>
         <div className="flex items-center gap-3">
@@ -219,7 +320,7 @@ const EditTitle = () => {
       </div>
       <Separator className="my-2" />
       <div className="flex flex-col m-1">
-        <label htmlFor="textAlign" className="text-sm">
+        <label htmlFor="textAlign" className="text-sm font-medium">
           Cor
         </label>
         <div
@@ -261,7 +362,189 @@ const EditTitle = () => {
           <span className={`w-4 h-4 rounded-full bg-${colors.accent}`}></span>
         </div>
       </div>
-    </div>
+      <Separator className="my-2" />
+      <div className="flex flex-col gap-2 m-1">
+        <label htmlFor="textAlign" className="text-sm font-medium">
+          Espaçamento
+        </label>
+        <div className="flex items-center justify-between">
+          <label htmlFor="paddingTop" className="text-sm">
+            Em cima
+          </label>
+          <select
+            id="paddingTop"
+            value={paddingTop}
+            onChange={(e) => {
+              setPaddingTop(e.target.value);
+              handlePaddingTop(e.target.value);
+            }}
+          >
+            <option value="pt-0">0px</option>
+            <option value="pt-1">4px</option>
+            <option value="pt-2">8px</option>
+            <option value="pt-3">12px</option>
+            <option value="pt-4">16px</option>
+            <option value="pt-5">20px</option>
+            <option value="pt-6">24px</option>
+            <option value="pt-7">28px</option>
+            <option value="pt-8">32px</option>
+            <option value="pt-9">36px</option>
+            <option value="pt-10">40px</option>
+            <option value="pt-11">44px</option>
+            <option value="pt-12">48px</option>
+            <option value="pt-14">56px</option>
+            <option value="pt-16">64px</option>
+            <option value="pt-20">80px</option>
+            <option value="pt-24">96px</option>
+            <option value="pt-28">112px</option>
+            <option value="pt-32">128px</option>
+            <option value="pt-36">144px</option>
+            <option value="pt-40">160px</option>
+            <option value="pt-44">176px</option>
+            <option value="pt-48">192px</option>
+            <option value="pt-52">208px</option>
+            <option value="pt-56">224px</option>
+            <option value="pt-60">240px</option>
+            <option value="pt-64">256px</option>
+            <option value="pt-72">288px</option>
+            <option value="pt-80">320px</option>
+            <option value="pt-96">384px</option>
+          </select>
+        </div>
+        <div className="flex items-center justify-between">
+          <label htmlFor="paddingRight" className="text-sm">
+            Direita
+          </label>
+          <select
+            id="paddingRight"
+            value={paddingRight}
+            onChange={(e) => {
+              setPaddingRight(e.target.value);
+              handlePaddingRight(e.target.value);
+            }}
+          >
+            <option value="pr-0">0px</option>
+            <option value="pr-1">4px</option>
+            <option value="pr-2">8px</option>
+            <option value="pr-3">12px</option>
+            <option value="pr-4">16px</option>
+            <option value="pr-5">20px</option>
+            <option value="pr-6">24px</option>
+            <option value="pr-7">28px</option>
+            <option value="pr-8">32px</option>
+            <option value="pr-9">36px</option>
+            <option value="pr-10">40px</option>
+            <option value="pr-11">44px</option>
+            <option value="pr-12">48px</option>
+            <option value="pr-14">56px</option>
+            <option value="pr-16">64px</option>
+            <option value="pr-20">80px</option>
+            <option value="pr-24">96px</option>
+            <option value="pr-28">112px</option>
+            <option value="pr-32">128px</option>
+            <option value="pr-36">144px</option>
+            <option value="pr-40">160px</option>
+            <option value="pr-44">176px</option>
+            <option value="pr-48">192px</option>
+            <option value="pr-52">208px</option>
+            <option value="pr-56">224px</option>
+            <option value="pr-60">240px</option>
+            <option value="pr-64">256px</option>
+            <option value="pr-72">288px</option>
+            <option value="pr-80">320px</option>
+            <option value="pr-96">384px</option>
+          </select>
+        </div>
+        <div className="flex items-center justify-between">
+          <label htmlFor="paddingBottom" className="text-sm">
+            Em baixo
+          </label>
+          <select
+            id="paddingBottom"
+            value={paddingBottom}
+            onChange={(e) => {
+              setPaddingBottom(e.target.value);
+              handlePaddingBottom(e.target.value);
+            }}
+          >
+            <option value="pb-0">0px</option>
+            <option value="pb-1">4px</option>
+            <option value="pb-2">8px</option>
+            <option value="pb-3">12px</option>
+            <option value="pb-4">16px</option>
+            <option value="pb-5">20px</option>
+            <option value="pb-6">24px</option>
+            <option value="pb-7">28px</option>
+            <option value="pb-8">32px</option>
+            <option value="pb-9">36px</option>
+            <option value="pb-10">40px</option>
+            <option value="pb-11">44px</option>
+            <option value="pb-12">48px</option>
+            <option value="pb-14">56px</option>
+            <option value="pb-16">64px</option>
+            <option value="pb-20">80px</option>
+            <option value="pb-24">96px</option>
+            <option value="pb-28">112px</option>
+            <option value="pb-32">128px</option>
+            <option value="pb-36">144px</option>
+            <option value="pb-40">160px</option>
+            <option value="pb-44">176px</option>
+            <option value="pb-48">192px</option>
+            <option value="pb-52">208px</option>
+            <option value="pb-56">224px</option>
+            <option value="pb-60">240px</option>
+            <option value="pb-64">256px</option>
+            <option value="pb-72">288px</option>
+            <option value="pb-80">320px</option>
+            <option value="pb-96">384px</option>
+          </select>
+        </div>
+        <div className="flex items-center justify-between">
+          <label htmlFor="paddingLeft" className="text-sm">
+            Esquerda
+          </label>
+          <select
+            id="paddingLeft"
+            value={paddingLeft}
+            onChange={(e) => {
+              setPaddingLeft(e.target.value);
+              handlePaddingLeft(e.target.value);
+            }}
+          >
+            <option value="pl-0">0px</option>
+            <option value="pl-1">4px</option>
+            <option value="pl-2">8px</option>
+            <option value="pl-3">12px</option>
+            <option value="pl-4">16px</option>
+            <option value="pl-5">20px</option>
+            <option value="pl-6">24px</option>
+            <option value="pl-7">28px</option>
+            <option value="pl-8">32px</option>
+            <option value="pl-9">36px</option>
+            <option value="pl-10">40px</option>
+            <option value="pl-11">44px</option>
+            <option value="pl-12">48px</option>
+            <option value="pl-14">56px</option>
+            <option value="pl-16">64px</option>
+            <option value="pl-20">80px</option>
+            <option value="pl-24">96px</option>
+            <option value="pl-28">112px</option>
+            <option value="pl-32">128px</option>
+            <option value="pl-36">144px</option>
+            <option value="pl-40">160px</option>
+            <option value="pl-44">176px</option>
+            <option value="pl-48">192px</option>
+            <option value="pl-52">208px</option>
+            <option value="pl-56">224px</option>
+            <option value="pl-60">240px</option>
+            <option value="pl-64">256px</option>
+            <option value="pl-72">288px</option>
+            <option value="pl-80">320px</option>
+            <option value="pl-96">384px</option>
+          </select>
+        </div>
+      </div>
+    </ScrollArea>
   );
 };
 
